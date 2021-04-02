a923ab04b3a629ae16f928c264efa7d11d4a348f
Alex Matsoukas <amatsoukas@olin.edu>
2021-04-02 19:30:13 -0400

    Update docstrings and reformat code to comply with pylint standards
    Make docstrings more descriptive and make code and import statements
    more readable by fixing pylint errors

--- google_cloud.py
+++ google_cloud.py
@@ -1,15 +1,15 @@
-import requests
 import json
 import os
 import pyjq
 import pandas as pd
+import requests
 import data_processing_helpers
 
-#path_lila = "/home/lila/Documents/schoolwork/softdes/google-api-key"
-path_alex = "/home/softdes/Desktop/google-api-key"
-api_path = "https://language.googleapis.com/v1/documents:analyzeSentiment?key="
+#PATH_LILA = "/home/lila/Documents/schoolwork/softdes/google-api-key"
+PATH_ALEX = "/home/softdes/Desktop/google-api-key"
+API_PATH = "https://language.googleapis.com/v1/documents:analyzeSentiment?key="
 
-with open(os.path.abspath(path_alex), "r") as f:
+with open(os.path.abspath(PATH_ALEX), "r") as f:
     api_key = f.readline()
 
 
@@ -26,7 +26,7 @@ def request_sentiment(text):
     "encodingType": "UTF32"
     }
 
-    response = requests.post(api_path + api_key, data=json.dumps(body))
+    response = requests.post(API_PATH + api_key, data=json.dumps(body))
     return response
 
 
@@ -42,7 +42,7 @@ def sentiment_and_magnitude_to_csv(country_name):
     """
     Conduct sentiment analysis on monthly headlines for a given country and
     update the country's data file with the corresponding scores
-    
+
     Args:
         country_name: a string representing the name of the country whose
         headlines will be analyzed
@@ -50,22 +50,22 @@ def sentiment_and_magnitude_to_csv(country_name):
         No return value
     """
     country_dataframe = pd.read_csv(f'CountryData/{country_name}_data.csv')
-    
+
     sentiment_scores = []
     magnitudes = []
-    
+
     for item in country_dataframe["MM-YYYY"]:
         year = item[3:]
         month = item[0:2]
         text = data_processing_helpers.headline_list_to_string(country_name, year + month)
         analysis = find_sentiment(request_sentiment(text))
-        
+
         sentiment_scores.append(analysis[0])
         magnitudes.append(analysis[1])
-        
+
     country_dataframe["Sentiment Score (-1 to 1)"] = sentiment_scores
     country_dataframe["Magnitude"] = magnitudes
-    
+
     updated_dataframe = country_dataframe
-    
+
     updated_dataframe.to_csv(f'CountryData/{country_name}_data.csv', index = False)
